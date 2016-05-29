c1feaf159970ce70e03bbaa80b049d8648b3d214
evgen1000end <evgen1000end@gmail.com>
2016-05-29 14:46:33 +0300

    add initial telegram bot

---
+++ src/main/java/demo/Main.java
@@ -0,0 +1,38 @@
+package demo;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import static spark.Spark.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //ipAddress(args[0]);
+       // port(Integer.parseInt(args[1]));
+
+        //ipAddress("79.120.40.196");
+        //port(9123);
+
+        // Bot handler
+        post("/myBot", new TestTelegramBot());
+
+        // simple string by GET
+        get("/hello", (req, res) -> "Hello World");
+
+        // GET/POST handler
+        post("/test", new Test());
+        get("/test", new Test());
+
+
+    }
+
+
+    private static class Test implements Route {
+        @Override
+        public Object handle(Request request, Response response) throws Exception {
+            return "ok, test!!!";
+        }
+    }
+}
